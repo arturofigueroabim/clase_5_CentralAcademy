d0539ce142daa5c59fa636c2fbb4d33cffcb024c
Arturo Figueroa <arturofigueroa@MacBook-Pro-de-Arturo.local>
2019-09-06 21:28:34 -0500

    ejercicio completo

---
+++ felicidadOHambre.js
@@ -0,0 +1,9 @@
+console.log("Dime Si tines hambre y te dire como te sientes");
+
+var  tenerHambre = false;
+
+if(tenerHambre){
+    console.log("Estas Triste :(")
+}else{
+    console.log("Estas Feliz :)")
+}
